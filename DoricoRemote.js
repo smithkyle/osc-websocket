f1ea6637cc050d75f756cac7334d2aafcc3a275d
Kyle Smith <smith.kyle2@gmail.com>
2024-11-10 14:01:41 -0800

    Dorico Remote added and working

---
+++ DoricoRemote.js
@@ -0,0 +1,68 @@
+const WebSocketClient = require('./WebSocketClient.js')
+
+class DoricoRemote extends WebSocketClient {
+    constructor(appName = 'Dorico Remote', handshakeVersion = '1.0') {
+        super('ws://127.0.0.1:4560');
+        this.appName = appName;
+        this.handshakeVersion = handshakeVersion;
+        this.sessionToken = null;
+        this.handshakeDone = false;
+    }
+
+    onOpen() {
+        this.sendHandshake();
+    }
+
+    sendHandshake() {
+        const message = {
+            message: 'connect',
+            clientName: this.appName,
+            handshakeVersion: this.handshakeVersion,
+        };
+        
+        if (this.sessionToken) {
+            message.sessionToken = this.sessionToken;
+        }
+        
+        this.sendMessage(message);
+    }
+
+    onMessage(data) {
+        if (data.message === 'sessiontoken' && data.sessionToken) {
+            this.sessionToken = data.sessionToken;
+            console.log(`Received sessiontoken: ${this.sessionToken} - completing handshake`);
+
+            const message = {
+                message: "acceptsessiontoken",
+                sessionToken: this.sessionToken
+            }
+
+            this.sendMessage(message);
+        }
+        else if (data.message === 'response' && data.code === 'kConnected') {
+            console.log(`Handshake complete for ${this.appName}`);
+            this.handshakeDone = true;  // Handshake is now complete
+            this.sendQueuedMessages();  // Now safe to send queued messages
+        }
+        
+        receive('/dorico', data)
+    }
+
+    onClose(event) {
+        if (event.code === 1000) {
+            console.log('Connection closed cleanly - send a command to open a new connection');
+            // this.sessionToken = null;
+            // this.handshakeDone = false;
+        } else {
+            console.log('Connection lost, retrying...');
+        }
+    }
+
+    close() {
+        if (this.socket?.readyState === WebSocket.OPEN) {
+            this.socket.close(1000, 'Unload');
+        }
+    }
+}
+
+module.exports = DoricoRemote;
--- customModule.js
+++ customModule.js
@@ -1,12 +1,16 @@
 const fs = nativeRequire('fs')
 
 const SibeliusConnect = require('SibeliusConnect.js')
+const DoricoRemote = require('DoricoRemote.js')
 
 module.exports = {
 
     init: function() {
-        global.remote = new SibeliusConnect()
-        global.remote.connect()
+        global.SibeliusConnect = new SibeliusConnect()
+        // global.SibeliusConnect.connect()
+
+        global.DoricoRemote = new DoricoRemote()
+        global.DoricoRemote.connect()
     },
 
     oscOutFilter: function(data) {
@@ -14,10 +18,15 @@ module.exports = {
         const { address, args, host, port, clientId } = data;
         
         if (address === '/sibeliusConnect') {
-            global.remote.sendMessage({
+            global.SibeliusConnect.sendMessage({
                 'message': 'invokeCommands',
                 'commands': [...args.map(arg => arg.value)]
             });
+        }
+        else if (address === '/doricoRemote') {
+            args.forEach(arg => {
+                global.DoricoRemote.sendMessage(JSON.parse(arg.value))
+            })
         }
         
         return { address, args, host, port };
